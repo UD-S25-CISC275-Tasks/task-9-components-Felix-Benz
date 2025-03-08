d0f909e051eaa551ebb756130c3ae72bd6583fa5
Felix Benz <fdbenz@udel.edu>
2025-03-07 23:27:53 -0500

    ChangeType passed tests

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,24 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): React.JSX.Element {
-    return <div>Change Type</div>;
+    const [questionType, setQuestionType] = useState<QuestionType>(
+        "short_answer_question"
+    );
+    function flipQuestionType(): void {
+        if (questionType == "short_answer_question") {
+            setQuestionType("multiple_choice_question");
+        } else {
+            setQuestionType("short_answer_question");
+        }
+    }
+    return (
+        <div>
+            <Button onClick={flipQuestionType}>Change Type</Button>
+            <div>
+                {questionType == "multiple_choice_question"
+                    ? "Multiple Choice"
+                    : "Short Answer"}
+            </div>
+        </div>
+    );
 }
