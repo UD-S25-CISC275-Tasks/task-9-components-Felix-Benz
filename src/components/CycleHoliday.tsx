908b6bf2c83d199ce4a6bb5ae86ab8a423263342
Felix Benz <fdbenz@udel.edu>
2025-03-09 15:23:25 -0400

    CycleHoliday almost done

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -2,5 +2,38 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function CycleHoliday(): React.JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [holiday, setHoliday] = useState<string>("🎄");
+    function advanceAlphabet(): void {
+        if (holiday == "🎄") {
+            setHoliday("🐰");
+        } else if (holiday == "🐰") {
+            setHoliday("🎃");
+        } else if (holiday == "🎃") {
+            setHoliday("🇺🇸");
+        } else if (holiday == "🇺🇸") {
+            setHoliday("🦃");
+        } else if (holiday == "🦃") {
+            setHoliday("🎄");
+        }
+    }
+    function advanceYear(): void {
+        if (holiday == "🎄") {
+            setHoliday("🐰");
+        } else if (holiday == "🐰") {
+            setHoliday("🇺🇸");
+        } else if (holiday == "🎃") {
+            setHoliday("🦃");
+        } else if (holiday == "🇺🇸") {
+            setHoliday("🎃");
+        } else if (holiday == "🦃") {
+            setHoliday("🎄");
+        }
+    }
+    return (
+        <div>
+            <h3>Holiday: {holiday}</h3>
+            <Button onClick={advanceAlphabet}>Advance by Alphabet</Button>
+            <Button onClick={advanceYear}>Advance by Year</Button>
+        </div>
+    );
 }
