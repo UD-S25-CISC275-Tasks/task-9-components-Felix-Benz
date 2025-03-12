be1c1b3932a7ac11cef219084962c54bf30598de
Felix Benz <fdbenz@udel.edu>
2025-03-11 23:38:11 -0400

    Fixed linting errors

--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -1,31 +1,5 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-import { dhValue, setDhValue } from "./DoubleHalfState";
-
-// function Doubler(): React.JSX.Element {
-//     return (
-//         <Button
-//             onClick={() => {
-//                 setDhValue(2 * dhValue);
-//             }}
-//         >
-//             Double
-//         </Button>
-//     );
-// }
-
-// function Halver(): React.JSX.Element {
-//     return (
-
-//         <Button
-//             onClick={() => {
-//                 setDhValue(0.5 * dhValue);
-//             }}
-//         >
-//             Halve
-//         </Button>
-//     );
-// }
 
 export function DoubleHalf(): React.JSX.Element {
     const [dhValue, setDhValue] = useState<number>(10);
@@ -36,8 +10,20 @@ export function DoubleHalf(): React.JSX.Element {
             <div>
                 The current value is: <span>{dhValue}</span>
             </div>
-            <Button onClick={() => setDhValue(dhValue * 2)}>Double</Button>
-            <Button onClick={() => setDhValue(dhValue * 0.5)}>Halve</Button>
+            <Button
+                onClick={() => {
+                    setDhValue(dhValue * 2);
+                }}
+            >
+                Double
+            </Button>
+            <Button
+                onClick={() => {
+                    setDhValue(dhValue * 0.5);
+                }}
+            >
+                Halve
+            </Button>
         </div>
     );
 }
